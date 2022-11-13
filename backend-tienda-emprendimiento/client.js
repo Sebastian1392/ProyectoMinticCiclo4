6c9fef4c38c0cc1c140e7bf011ad5b5187286700
Daniel Martinez <78056789+pepesurikta@users.noreply.github.com>
2022-11-12 21:55:00 -0500

    cart creation for backend and frontend

--- backend-tienda-emprendimiento/client.js
+++ backend-tienda-emprendimiento/client.js
@@ -2,44 +2,75 @@
 // const cors = require('cors');
 // const path = require('path');
 // const dataBase = require('./data');
-const express = require('express'); //se importa el marco de trabajo
-const mongoose = require('mongoose')
-const Product = require('./product');
+const express = require("express"); //se importa el marco de trabajo
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Product = require("./product");
+const { request } = require("express");
 
 const app = express(); //se crea una instancia de la aplicación express
 const port = 3001; // se configura el puerto
-const url = "mongodb+srv://theencodingteam:theencodingteam@clusterproductos.rzruckt.mongodb.net/tiendaEmprendimiento?retryWrites=true&w=majority";
+const url =
+	"mongodb+srv://theencodingteam:theencodingteam@clusterproductos.rzruckt.mongodb.net/tiendaEmprendimiento?retryWrites=true&w=majority";
 
-let carProducts = []
+let cartProducts = [];
+const baseUrl = "/api/v1/client";
 
 app.use(express.json());
+app.use(
+	cors({
+		origin: "*",
+	})
+);
 
-mongoose.connect(url)
-    .then(()=>console.log("conectado a mongodb"))
-    .catch(e=>console.log("error de conexión",e));
+mongoose
+	.connect(url)
+	.then(() => console.log("conectado a mongodb"))
+	.catch((e) => console.log("error de conexió:  ", e));
 
-
-app.get('/productos/client', async (req, res) => {
-    const productList = await Product.find({stock: {$gt : 0}});
-    console.log(productList);
-    try {
-        res.send(productList);
-    } catch (error) {
-        console.log("error", error)
-    }
+app.get(`${baseUrl}/products`, async (req, res) => {
+	try {
+		const productList = await Product.find({ stock: { $gt: 0 } });
+		console.log(productList);
+		res.send(productList);
+	} catch (error) {
+		console.log("error: ", error);
+	}
 });
 
-app.get('/agregar-producto/client/', async (req, res) => {
-    const clientRequest = req.body
-    try {
-        const product = await Product.findOne({ _id: clientRequest.id });
-        carProducts.push({ product: product, quantity:clientRequest.quantity});
-        res.send(carProducts);
-    } catch (error) {
-        console.log("error", error)
-    }
+app.get(`${baseUrl}/products/:id`, async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.id);
+		res.send(product);
+	} catch (error) {
+		console.log("error: ", error);
+	}
 });
 
+app.get(`${baseUrl}/cart`, (req, res) => {
+	res.send(cartProducts);
+});
 
+app.post(`${baseUrl}/cart/addProduct`, (req, res) => {
+	const {id} = req.body;
+    if(cartProducts.some( product => id === product.id)) {
+        const index = cartProducts.findIndex(product => id === product.id);
+        cartProducts[index].quantity += 1;
+    } else {
+        cartProducts.push({ id, quantity: 1 });
+    }
+    console.log(cartProducts);
+	res.send(cartProducts);
+});
 
-app.listen(port,()=>console.log(`App Libro está en el puerto ${port}!`));
+app.put(`${baseUrl}/cart/purchase`, async (req, res) => {
+	await Promise.all(cartProducts.map(async ({id, quantity}) => {
+        const product = await Product.findById(id);
+        const stock = product.stock - quantity;
+        return Product.findOneAndUpdate({_id: id}, {stock});
+    }));
+    cartProducts = [];
+    res.send(cartProducts);
+});
+
+app.listen(port, () => console.log(`App Libro está en el puerto ${port}!`));
--- tienda-emprendimiento/src/components/products/car-products.js
+++ tienda-emprendimiento/src/components/products/car-products.js
@@ -1,45 +1,100 @@
-import React from "react";
-import styles from '../../styles/car-products.module.css';
-import sol from "../../img/sol.png"
-import corazon from "../../img/corazon.png"
+import React, { useEffect, useState } from "react";
+import styles from "../../styles/car-products.module.css";
+import sol from "../../img/sol.png";
+import corazon from "../../img/corazon.png";
 
-export const CarProducts = () => { 
-    return (
-        <>
-            <table>
-                <tr>
-                    <th>Imagen</th>
-                    <th>Cantidad</th>
-                    <th>Producto</th>
-                    <th>Valor</th>
-                    <th>Total</th>
-                </tr>
-                <tr>
-                    <td><img alt="sol" src={sol} /></td>
-                    <td>2</td>
-                    <td>Sol de espuma</td>
-                    <td>$ 50</td>
-                    <td>$ 100</td>
-                </tr>
-                <tr>
-                    <td><img alt="sol" src={corazon}/></td>
-                    <td>1</td>
-                    <td>Corazón juguete</td>
-                    <td>$ 50</td>
-                    <td>$ 50</td>
-                </tr>
-                <tr>
-                    <td className={styles.total}></td>
-                    <td className={styles.total}></td>
-                    <td className={styles.total}></td>
-                    <td className={styles.total}><b>Total</b></td>
-                    <td className={styles.total}><b>$ 150</b></td>
-                </tr>
-            </table>
-            <div className={styles.buttonsDiv}>
-                <button>Finalizar Compra</button>
-                <button>Cancelar</button>
-            </div>
-        </>
-    );
-}
+export const CarProducts = () => {
+	const [cart, setCart] = useState(false);
+
+    const getProduct = async (id, quantity) => {
+        const response = await fetch(`http://localhost:3001/api/v1/client/products/${id}`);
+        const { name, price } = await response.json();
+        return {
+            id,
+            image: "",
+            name,
+            price,
+            quantity,
+            total: price * quantity,
+        };
+    };
+
+    const getCart = async () => {
+        const response = await fetch("http://localhost:3001/api/v1/client/cart");
+        const cartResponse = await response.json();
+        const cart = await Promise.all(cartResponse.map(async ({id, quantity}) => {
+            return await getProduct(id, quantity);
+        }));
+        setCart(cart);
+    };
+
+    const onPurchase = async() => {
+        const response = await fetch("http://localhost:3001/api/v1/client/cart/purchase", {
+            method: 'PUT'
+        });
+        const cart = await response.json();
+        setCart(cart);
+    };
+
+
+	useEffect(() => {
+		getCart()
+	}, []);
+	if (!cart) {
+		return <>cargando...</>;
+	} else {
+		return (
+			<>
+				<table>
+					<thead>
+						<tr>
+							<th>Imagen</th>
+							<th>Cantidad</th>
+							<th>Nombre del producto</th>
+							<th>Valor</th>
+							<th>Total</th>
+						</tr>
+					</thead>
+					<tbody>
+						{cart.map(
+							({ id, image, quantity, name, price, total }) => {
+								return (
+									<tr key={id}>
+										<td>
+											<img src={image} />
+										</td>
+										<td>{quantity}</td>
+										<td>{name}</td>
+										<td>$ {price}</td>
+										<td>$ {total}</td>
+									</tr>
+								);
+							}
+						)}
+					</tbody>
+					<tfoot>
+						<tr>
+							<td className={styles.total}></td>
+							<td className={styles.total}></td>
+							<td className={styles.total}></td>
+							<td className={styles.total}>
+								<b>Total</b>
+							</td>
+							<td className={styles.total}>
+								<b>$ {
+                                    Math.round(cart.reduce((previous, {total}) => previous + total, 0))
+                                }</b>
+							</td>
+						</tr>
+					</tfoot>
+				</table>
+				<div className={styles.buttonsDiv}>
+					<button
+                    onClick={() => onPurchase()}
+                    >Finalizar Compra</button>
+					<button>Cancelar</button>
+				</div>
+			</>
+		);
+	}
+};
